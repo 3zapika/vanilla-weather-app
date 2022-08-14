b587029301dccda3e02c32eb4e3606b3f1a68f5f
Inna Zaporozhets <innchukzapik@gmail.com>
2022-08-14 20:32:06 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,2 +1,8 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+}
 let apiKey = "e4d1a34968ab1f1e4fe14d8fde838d98";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid={apiKey}`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q="Oslo"&appid={apikey}&units=metric`;
+axios.get(apiUrl).then(displayTemperature);
